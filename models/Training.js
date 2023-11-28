b4e09fe474619c3938f09e4fbd0c02d0867e3cf9
szalaibator <szalai.arpad.bator@students.jedlik.eu>
2023-11-28 09:35:55 +0100

    geocoder added

--- models/Training.js
+++ models/Training.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const geocoder = require("../utils/geocoder");
 const TrainingSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -83,4 +84,23 @@ const TrainingSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+TrainingSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].state,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  }; // Formázott címünk lesz,
+  // nincs szükség
+  // az eredeti címre
+  this.address = undefined;
+  next();
+});
+
 module.exports = mongoose.model("Training", TrainingSchema, "trainings");
